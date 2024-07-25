10aa50c30a93dc36863f15faab3ec96a2e33f08e
kunal latta <95677039+KUNALLATTA29@users.noreply.github.com>
2024-07-25 22:55:11 +0530

    prog_4.js

---
+++ prog_4.js
@@ -0,0 +1,47 @@
+const arr = [
+    {
+        age:23,
+        name: "kunal",
+        sub: "math",
+        amount:50,
+        id : 1
+},
+ {
+        age:25,
+        name: "chunu",
+        sub: "english",
+         amount:10,
+         id : 2
+},
+{
+        age:26,
+        name: "kaliya",
+        sub: "hindi",
+        amount:40,
+        id : 3
+},
+{
+        age:13,
+        name: "bheem",
+        sub: "sst",
+        amount:80,
+        id : 4
+},
+{
+        age:27,
+        name: "damru",
+        sub: "science",
+        amount:40,
+        id : 5
+},
+{
+        age:12,
+        name: "mali",
+        sub: "computer",
+        amount:70,
+        id : 6
+}
+]
+
+console.log(arr.map(ele => ele.id));
+    
