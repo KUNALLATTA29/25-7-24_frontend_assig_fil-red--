a282b59c7c44aa4ee6f4c5a8c14a5a18740e25f5
kunal latta <95677039+KUNALLATTA29@users.noreply.github.com>
2024-07-25 22:59:06 +0530

    prog_5.js

---
+++ prog_5.js
@@ -0,0 +1,53 @@
+const arr = [
+    {
+        age:23,
+        name: "kunal",
+        sub: "math",
+        amount:50,
+        id : 1,
+        status: "pen"
+},
+ {
+        age:25,
+        name: "chunu",
+        sub: "english",
+         amount:10,
+         id : 2,
+         status: "com"
+},
+{
+        age:26,
+        name: "kaliya",
+        sub: "hindi",
+        amount:40,
+        id : 3,
+        status: "com"
+},
+{
+        age:13,
+        name: "bheem",
+        sub: "sst",
+        amount:80,
+        id : 4,
+        status: "pen"
+},
+{
+        age:27,
+        name: "damru",
+        sub: "science",
+        amount:40,
+        id : 5,
+        status: "com"
+},
+{
+        age:12,
+        name: "mali",
+        sub: "computer",
+        amount:70,
+        id : 6,
+        status: "pen"
+}
+]
+
+console.log(arr.filter(ele => ele.status==="pen"));
+    
