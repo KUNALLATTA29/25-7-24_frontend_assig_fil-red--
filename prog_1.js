40f2a0c2d2858dc61cc5ba4cfdc8503467cd0416
kunal latta <95677039+KUNALLATTA29@users.noreply.github.com>
2024-07-25 22:45:39 +0530

    prog_1.js

---
+++ prog_1.js
@@ -0,0 +1,35 @@
+const arr = [
+    {
+        age:23,
+        name: "kunal",
+        sub: "math"
+},
+ {
+        age:25,
+        name: "chunu",
+        sub: "english"
+},
+{
+        age:26,
+        name: "kaliya",
+        sub: "hindi"
+},
+{
+        age:13,
+        name: "bheem",
+        sub: "sst"
+},
+{
+        age:27,
+        name: "damru",
+        sub: "science"
+},
+{
+        age:12,
+        name: "mali",
+        sub: "computer"
+}
+]
+
+console.log(arr.filter(ele => ele.age<=25));
+    
